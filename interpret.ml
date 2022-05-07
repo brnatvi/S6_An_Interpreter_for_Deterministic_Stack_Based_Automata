5ff6cce1e9070dcf2428c40c7d6a9c51ca55f2f5
BRAGINA natalia <nata.br@gmail.com>
2022-05-07 22:52:38 +0200

    it works but not the way it should work

--- interpret.ml
+++ interpret.ml
@@ -1,7 +1,11 @@
 open Ast
 
 exception EmptyTransitionList of string
-exception EmptyList of string
+exception Empty of string
+exception InitialStateCorrupted of string
+exception InitialStackCorrupted of string
+exception EmptyStackException
+exception Error
 
 (* ------------------------- Auxilairy functions ----------------------------- *)
 
@@ -18,33 +22,104 @@ let rec append l i =
 (* GENERIC: pull last element from list *)
 let rec pull_list l =
   match l with
-  | [] -> raise (EmptyList "There is nothing to pull cause list is empty")
+  | [] -> raise (Empty "There is nothing to pull cause list is empty")
   | [i] -> i
   | h::tail -> pull_list tail
 
-
+(* GENERIC: delete last element from list, return list *)
+let list_without_last l =
+  let rev_list = List.rev l in
+  match rev_list with
+  | [] -> raise (Empty "List is empty")
+  | [i] -> []
+  | h::tail -> List.rev tail
+  
 (* ------------------------- Service functions ----------------------------- *)
 
 (* check if stack empty*)
 let is_stack_empty (l: transition list) : bool = failwith "TODO"
   
+(* pull last element from nonemptystack *)
+let rec pull_from_nonemptystack (ne: nonemptystack) : char =
+  match ne with 
+  | Lettre l -> 
+    (match l with
+    | Digit i -> raise (Error)
+    | Upper c -> c
+    | Lower c -> c
+    )
+  | Nonemptystack (l, s) -> pull_from_nonemptystack s
+
+(* pull last element from stack *)
+let pull_from_stack (st: stack) : char =
+  match st with
+  | Emptystack -> raise (EmptyStackException)
+  | Stack (ne) -> pull_from_nonemptystack ne
 
 
-  
+let compare_char_lettre_ou_vide (st : lettre_ou_vide) (l: char) : bool =
+  match st with  
+    | None -> false
+    | Some letter -> 
+      (match letter with
+      | Digit i -> raise (Error)
+      | Upper c -> if c = l then true else false
+      | Lower c -> if c = l then true else false
+      )
+ 
+let compare_char_lettre (st : lettre) (l: char) : bool =
+  match st with
+    | Digit i -> raise (Error)
+    | Upper c -> if c = l then true else false
+    | Lower c -> if c = l then true else false
+
+
+    
+let get_char (st : lettre) : char =
+  match st with
+    | Digit i -> raise (Error)
+    | Upper c -> c
+    | Lower c -> c
 
 (* ------------------------- Main function ----------------------------- *)
 
 let execute_automate (a : automate) (word : char list) : unit =
   let Automate (decl, trs) = a in
-  let Transitions (trans_list) = trs in
-  (match trans_list with
-  | Emptylist -> raise (EmptyTransitionList ("Automate can't execute any word cause have not any transition"))
-  | Translist (l) ->     
-      (match l with
-      | [] -> (*check if stack is empty and word = "" *) failwith "TODO"
-      | x::xs -> failwith "TODO"
-      
-        (* let Transition (l1, l_ou_v, l2, l3, st) = x *)
-          
+  let Declarations (in_symb, st_symb, st, in_state, in_stack) = decl in
+  
+  let Initialstate (in_st) = in_state in  (* lettre *)
+  let Initialstack (in_stck) = in_stack in  (* lettre *)
+
+  let Transitions (trans_list) = trs in  
+  (match trans_list, word with  
+  | Emptylist, _ -> raise (EmptyTransitionList ("Automate can't execute any word cause have not any transition"))
+  | Translist (liste), word -> 
+    let rec aux trans_l word curr_state curr_stack =
+      (match trans_l with
+        | [] -> if (curr_stack = [] && word = []) then print_string "PERFECT !!" (*check if stack is empty and word = "" *)
+                else print_string "NOT GOOD (("
+        | first_tr::rest_l -> 
+          let Transition (depart, l_ou_v, nonterm, arrive, stack) = first_tr in
+
+          (match word with
+          | [] -> raise (Empty "This is empty word")
+          | w::rest_word -> 
+            if (curr_state != depart) then raise(InitialStateCorrupted("Initial state != initial state of first transition"))
+              else 
+                (
+                  if (compare_char_lettre (nonterm) (pull_list curr_stack)) then raise(InitialStackCorrupted("Initial stack != initial stack of first transition"))
+                  else
+                    (
+                      if (compare_char_lettre_ou_vide (l_ou_v) (w)) then aux rest_l word curr_state curr_stack      (* continue with same letter and the rest of transitions *)
+                      else
+                        (                          
+                          let new_stack = (try (append curr_stack (pull_from_stack stack)) with EmptyStackException -> list_without_last curr_stack) in  
+                          let new_state = arrive in
+                          aux rest_l rest_word new_state new_stack
+                        )
+                    )
+                )
+          )
       )
-  )   
+    in aux liste word in_st [get_char in_stck]
+  )
--- main.ml
+++ main.ml
@@ -1,10 +1,11 @@
 open Prints
+open Interpret
 
 let usage () =
-  print_string "Usage: ./grammaire [option] pathfile \n\n";
+  print_string "Usage: ./grammaire [option] file [word] \n\n";
   print_string "Options: \n";
   print_string "--reprint     - compose abstract syntax tree based input file and reprint it\n";
-  print_string "--interpret   - execute automate on word\n"
+  print_string "--interpret   - execute automate based input file on word\n"
 
 
 let main () =
@@ -32,7 +33,30 @@ let main () =
             )
           )
 
-        | [|_;"--interpret";file;word|] -> failwith "TODO"
+        | [|_;"--interpret";file;word|] -> 
+          (
+            let ch = open_in file in
+
+            let lexbuf = Lexing.from_channel ch in
+
+            let ast = try
+              Some (Grammaire.automate Lexer.lexer lexbuf)
+              with
+                | msg -> (
+                  print_string (Printexc.to_string msg); print_string "\n";
+                  let pos = Lexing.lexeme_start_p lexbuf in
+                  Printf.fprintf stdout "At line %d, offset %d\n%!" pos.pos_lnum (pos.pos_cnum - pos.pos_bol);
+                  None
+                )
+              in
+          
+            (match ast with
+              | Some a -> Interpret.execute_automate a (word_to_list word)      
+              | None -> ()
+            )
+          )
+                    
+          
         | _ -> usage()
   )
 
